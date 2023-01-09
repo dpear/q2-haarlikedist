647226c0c2dcefa9637e4e07c387425782c24aa6
Daniela Perry <danielaperry2015@gmail.com>
2023-01-09 14:58:15 -0800

    add all test cases for sparsification

--- q2_haarlikedist/_methods.py
+++ q2_haarlikedist/_methods.py
@@ -6,48 +6,205 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-# import pandas as pd
-# from scipy.spatial import ConvexHull
-# from skbio import OrdinationResults
-# from q2_convexhull._defaults import (DEFAULT_N_DIMENSIONS)
-# from warnings import warn
-# from qiime2 import Metadata
-from ete3 import Tree
-import pkg_resources
-import q2templates
-import os
+import scipy
+import skbio
+import numpy as np 
+from io import StringIO
+from skbio import read
+
+def get_tree_from_file(tree_file):
+
+    f = open(tree_file, 'r')
+    t2 = read(f, format='newick', into=TreeNode)
+    f.close()
+    return t2
 
 
-def haar_like_dist(output_dir: str) -> None:
+def initiate_values(t2):
+    """ Returns t2, shl, lilmat.
+    shl: Matrix that tracks all haar-like vectors
+    SHL = SPARSE HAAR LIKE 
+    Rows = internal nodes; Columns = tips with values of :
+       -- 0 if tip is not a descendant from internal node
+       -- c_L if tip is a left descendant from internal node
+       -- c_R if tip is a right descendant from internal node
+    c_L and c_R are calculated in the get_L function.
 
-    """ Computes Haar Like Distance between two
-        samples by projecting their phylogenies
-        onto a Haar-like wavelet space.
+    lilmat: Matrix that tracks l-star values. n tips x n nontips (so square)
+    Entry i, j represents weighted path distance from internal node i to tip j
+    This is why it is computed iteratively for internal nodes 
+    with non-tip descendants. """
 
-    Parameters
-    ----------
-    table: FeatureTable 
+    ntips = len([x for x in t2.tips()])
+    shl = scipy.sparse.lil_matrix((ntips, ntips))
+    lilmat = scipy.sparse.lil_matrix((ntips, ntips))
 
-    phylogeny: UM
+    tip_index = {t: i for i, t in enumerate(t2.tips())}
 
-    Returns
-    -------
-    output.qzv:
-        visualization which contains a downloadable
-        distance matrix as well as vectors
-        which correspond to importance of 
-        difference.
+    for i, node in enumerate(t2.non_tips(include_self=True)):
+        node.postorder_pos = i
 
-    Raises
-    ------
-    TypeError, ValueError
-        If inputs are of incorrect type. If column ID not
-        found in metadata.
-    """
+    for node in t2.postorder(include_self=True):
+        node.ntips_in_tree = ntips
+        node.tip_names = [tip_index[x] for x in node.tips()]
 
-    t = Tree()
+        if node.is_tip():
+            node.tip_names = [tip_index[node]]
 
-    TEMPLATES = pkg_resources.resource_filename(
-        'q2_haarlikedist', 'haar_like_dist_assets')
-    index = os.path.join(TEMPLATES, 'index.html')
-    q2templates.render(index, output_dir)
+    return t2, shl, lilmat
+
+
+def get_case(node):
+    """ Handle different types of children differently. 
+        4 possible child layouts; returns string describing the case.
+        Neither => neither child is a tip etc. """
+
+    left_is_tip = not node.children[0].has_children()
+    right_is_tip = not node.children[1].has_children()
+
+    if not left_is_tip and not right_is_tip:
+        return 'neither'
+
+    if left_is_tip and right_is_tip:
+        return 'both'
+
+    if left_is_tip and not right_is_tip:
+        return 'left'
+
+    if not left_is_tip and right_is_tip:
+        return 'right'
+        
+
+def get_nontip_index(node, side):
+    """ Returns a single nontip index of the node's left or right child. 
+        Only valid for a nontip node and nontip child. """
+
+    ind = 0 if side == 'left' else 1
+    return node.children[ind].postorder_pos
+
+
+def get_tip_indeces(node, side):
+    """ Returns all left or right tip children. """
+
+    ind = 0 if side == 'left' else 1
+    return node.children[ind].tip_names
+
+
+def get_lstar(child, tip_inds, nontip_inds, lilmat):
+    """ Returns lstar, which is the cumulative modified branch lengths
+        from each internal node to each tip node (entries in lstar). """
+
+    if nontip_inds == None:
+        ntips = child.ntips_in_tree
+        lstar = np.zeros((ntips, 1))
+    else:
+        lstar = lilmat[nontip_inds].todense().T
+
+    lstar[tip_inds] = lstar[tip_inds] + len(tip_inds) * child.length
+
+    return lstar
+
+
+def get_L(tip_inds0, tip_inds1):
+    """ Returns the values that each left and right side of the haarvec
+        should be set to. These are the heights of the wavelets """
+
+    L0 = len(tip_inds0)
+    L1 = len(tip_inds1)
+
+    left = np.sqrt(L1/(L0*(L0+L1)))
+    right = - np.sqrt(L0/(L1*(L0+L1)))
+
+    return left, right
+
+
+def get_haarvec(tip_inds0, tip_inds1, left, right, ntips):
+    """ Returns the haarlike wavelets. """
+
+    haarvec = np.zeros((ntips, 1))
+
+    haarvec[tip_inds0] = left
+    haarvec[tip_inds1] = right
+
+    return haarvec
+
+
+def get_lilmat_and_shl(node, nontip_inds0, nontip_inds1, lilmat, shl, i):
+    """ Processes the nontip indeces and updates lilmat and shl
+        matrices. Is performed in each of the 4 cases. """
+
+    tip_inds0 = get_tip_indeces(node, 'left')
+    tip_inds1 = get_tip_indeces(node, 'right')
+
+    lstar0 = get_lstar(node.children[0], tip_inds0, nontip_inds0, lilmat)
+    lstar1 = get_lstar(node.children[1], tip_inds1, nontip_inds1, lilmat)
+
+    lilmat[i] = lstar0.T + lstar1.T
+
+    ntips = node.ntips_in_tree
+    left, right = get_L(tip_inds0, tip_inds1)
+    haarvec = get_haarvec(tip_inds0, tip_inds1, left, right, ntips)
+    shl[i] = haarvec.T
+
+    return lilmat, shl
+
+
+def handle_neither(node, lilmat, shl, i):
+    """ Case where neither child is a tip. """
+
+    # rows of lilmat indexed by nontips - select row to modify
+    nontip_inds0 = get_nontip_index(node, 'left')
+    nontip_inds1 = get_nontip_index(node, 'right')
+
+    return get_lilmat_and_shl(node, nontip_inds0, nontip_inds1, lilmat, shl, i)
+
+
+def handle_left(node, lilmat, shl, i):
+    """ Case where neither left is a tip. """
+
+    nontip_inds0 = None # Doesn't have a row index in lilmat
+    nontip_inds1 = get_nontip_index(node, 'right')
+
+    return get_lilmat_and_shl(node, nontip_inds0, nontip_inds1, lilmat, shl, i)
+
+
+def handle_right(node, lilmat, shl, i):
+    """ Case where right child is a tip. """
+
+    nontip_inds0 = get_nontip_index(node, 'left')
+    nontip_inds1 = None
+
+    return get_lilmat_and_shl(node, nontip_inds0, nontip_inds1, lilmat, shl, i)
+
+
+def handle_both(node, lilmat, shl, i):
+    """ Case where both children are tips. """
+
+    nontip_inds0 = None
+    nontip_inds1 = None
+
+    return get_lilmat_and_shl(node, nontip_inds0, nontip_inds1, lilmat, shl, i)
+
+
+def sparsify(tree_file):
+
+    t2 = get_tree_from_file(tree_file)
+    t2, shl, lilmat = initiate_values(t2)
+
+    traversal = t2.non_tips(include_self=True)
+    for i, node in enumerate(traversal):
+        case = get_case(node)
+
+        if case == 'neither':
+            lilmat, shl = handle_neither(node, lilmat, shl, i)
+
+        elif case == 'both':
+            lilmat, shl = handle_both(node, lilmat, shl, i)
+            
+        elif case == 'left':
+            lilmat, shl = handle_left(node, lilmat, shl, i)
+
+        elif case == 'right':
+            lilmat, shl = handle_right(node, lilmat, shl, i)
+
+    return lilmat, shl
--- q2_haarlikedist/tests/test_methods.py
+++ q2_haarlikedist/tests/test_methods.py
@@ -4,19 +4,309 @@ import os
 from unittest import TestCase, main
 from os.path import dirname, abspath, join
 from inspect import currentframe, getfile
-from q2_haarlikedist._methods import haar_like_dist
+from q2_haarlikedist._methods import *
 
 
-class TestHaarLikeDist(TestCase):
+class TestSparsify(TestCase):
 
-    def test_haar_like_dist(self):
+    def setUp(self):
+        self.ntips = 7
+        self.tree_file = 'small-tree.tree'
 
-        with tempfile.TemporaryDirectory() as output_dir:
-
-            index_fp = os.path.join(output_dir, 'index.html')
-
-            haar_like_dist(output_dir)
-            self.assertTrue(os.path.exists(index_fp))
+        self.lilmat = scipy.sparse.lil_matrix((self.ntips, self.ntips))
+        self.shl = scipy.sparse.lil_matrix((self.ntips, self.ntips))
 
 
+    def test_get_tree_from_file(self):
 
+        t2 = get_tree_from_file(self.tree_file)
+        assert(t2.count() == 13)
+
+
+    def test_initiate_values(self):
+
+        expected = scipy.sparse.lil_matrix((self.ntips, self.ntips))
+
+        t2 = get_tree_from_file(self.tree_file)
+        t2, lilmat, shl = initiate_values(t2)
+
+        assert((lilmat != expected).nnz == 0)
+        assert((shl != expected).nnz == 0)
+
+        for i, node in enumerate(t2.non_tips(include_self=True)):
+            assert(node.postorder_pos == i)
+
+        for i, node in enumerate(t2.postorder(include_self=True)):
+
+            assert(node.ntips_in_tree == 7)
+            
+            if i == 0:
+                assert(node.tip_names == [0])
+            if i == 1:
+                assert(node.tip_names == [1])
+            if i == 2:
+                assert(node.tip_names == [2])
+            if i == 3:
+                assert(node.tip_names == [1, 2])
+            if i == 4:
+                assert(node.tip_names == [0, 1, 2])
+            if i == 11:
+                assert(node.tip_names == [3, 4, 5, 6])
+            if i == 12:
+                assert(node.tip_names == [0, 1, 2, 3, 4, 5, 6])
+
+    def test_get_case(self):
+
+        t2 = get_tree_from_file(self.tree_file)
+        t2, lilmat, shl = initiate_values(t2)
+
+        for i, node in enumerate(t2.non_tips(include_self=True)):
+
+            if i == 0:
+                assert(get_case(node) == 'both')
+            if i == 1:
+                assert(get_case(node) == 'left')
+            if i == 2:
+                assert(get_case(node) == 'both')
+            if i == 3:
+                assert(get_case(node) == 'right')
+            if i == 4:
+                assert(get_case(node) == 'left')
+            if i == 5:
+                assert(get_case(node) == 'neither')
+
+
+    def test_get_nontip_index(self):
+
+        t2 = get_tree_from_file(self.tree_file)
+        t2, lilmat, shl = initiate_values(t2)
+        
+        for i, node in enumerate(t2.non_tips(include_self=True)):
+            
+            if i == 0: # case = both, neither children nontips
+                continue 
+            if i == 1: # case = left, right child is nontipi
+                assert(get_nontip_index(node, 'right') == 0)
+            if i == 2: # case = both
+                continue
+            if i == 3: # case = right
+                assert(get_nontip_index(node, 'left') == 2)
+            if i == 4: # case = left
+                assert(get_nontip_index(node, 'right') == 3)
+            if i == 5: # case = neither
+                assert(get_nontip_index(node, 'left') == 1)
+                assert(get_nontip_index(node, 'right') == 4)
+
+    def test_get_tip_indeces(self):
+
+        t2 = get_tree_from_file(self.tree_file)
+        t2, lilmat, shl = initiate_values(t2)
+
+        for i, node in enumerate(t2.non_tips(include_self=True)):
+
+            if i == 0:
+                assert(get_tip_indeces(node, 'left') == [1])
+                assert(get_tip_indeces(node, 'right') == [2])
+            if i == 1:
+                assert(get_tip_indeces(node, 'left') == [0])
+                assert(get_tip_indeces(node, 'right') == [1, 2])
+            if i == 2:
+                assert(get_tip_indeces(node, 'left') == [4])
+                assert(get_tip_indeces(node, 'right') == [5])
+            if i == 3:
+                assert(get_tip_indeces(node, 'left') == [4, 5])
+                assert(get_tip_indeces(node, 'right') == [6])
+            if i == 4:
+                assert(get_tip_indeces(node, 'left') == [3])
+                assert(get_tip_indeces(node, 'right') == [4, 5, 6])
+            if i == 5:
+                assert(get_tip_indeces(node, 'left') == [0, 1, 2])
+                assert(get_tip_indeces(node, 'right') == [3, 4, 5, 6])
+
+    def test_get_lstar(self):
+
+        t2 = get_tree_from_file(self.tree_file)
+        t2, lilmat, shl = initiate_values(t2)
+
+        for i, node in enumerate(t2.non_tips(include_self=True)):
+            
+            if i == 0:
+                expected = np.matrix([[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
+                tip_inds0 = get_tip_indeces(node, 'left')
+                nontip_inds0 = get_tip_indeces(node, 'left')
+                l = get_lstar(node.children[0], tip_inds0, nontip_inds0, lilmat)
+                assert((l.T == expected).all())
+
+                expected = np.matrix([[0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0]])
+                tip_inds1 = get_tip_indeces(node, 'right')
+                nontip_inds1 = get_tip_indeces(node, 'right')
+                l = get_lstar(node.children[1], tip_inds1, nontip_inds1, lilmat)
+                assert((l.T == expected).all())
+
+            if i == 4:
+
+                expected = np.matrix([[0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]])
+                tip_inds0 = get_tip_indeces(node, 'left')
+                nontip_inds0 = get_tip_indeces(node, 'left')
+                l = get_lstar(node.children[0], tip_inds0, nontip_inds0, lilmat)
+                assert(np.isclose(l.T, expected).all())
+
+                expected = [[0.0, 0.0, 0.0, 0.0, 3.0, 3.0, 3.0],
+                            [0.0, 0.0, 0.0, 0.0, 3.0, 3.0, 3.0],
+                            [0.0, 0.0, 0.0, 0.0, 3.0, 3.0, 3.0]]
+                tip_inds1 = get_tip_indeces(node, 'right')
+                nontip_inds1 = get_tip_indeces(node, 'right')
+                l = get_lstar(node.children[1], tip_inds1, nontip_inds1, lilmat)
+                assert(np.isclose(l.T, expected).all())
+
+    def test_get_L(self):
+
+        t2 = get_tree_from_file(self.tree_file)
+        t2, lilmat, shl = initiate_values(t2)
+
+        for i, node in enumerate(t2.non_tips(include_self=True)):
+
+            if i == 0:
+                tip_inds0 = get_tip_indeces(node, 'left')
+                tip_inds1 = get_tip_indeces(node, 'right')
+                left, right = get_L(tip_inds0, tip_inds1)
+                assert(np.isclose(left, np.sqrt(1/2)))
+                assert(np.isclose(right, -np.sqrt(1/2)))
+
+            if i == 3:
+                tip_inds0 = get_tip_indeces(node, 'left')
+                tip_inds1 = get_tip_indeces(node, 'right')
+                left, right = get_L(tip_inds0, tip_inds1)
+                assert(np.isclose(left, np.sqrt(1/6)))
+                assert(np.isclose(right, -np.sqrt(2/3)))
+
+    def test_get_haarvec(self):
+
+        t2 = get_tree_from_file(self.tree_file)
+        t2, lilmat, shl = initiate_values(t2)
+
+        for i, node in enumerate(t2.non_tips(include_self=True)):
+            
+            tip_inds0 = get_tip_indeces(node, 'left')
+            tip_inds1 = get_tip_indeces(node, 'right')
+            left, right = get_L(tip_inds0, tip_inds1)
+            h = get_haarvec(tip_inds0, tip_inds1, left, right, node.ntips_in_tree)
+            assert(np.isclose(sum(h), 0.0)) # The sum of haar vectors should be 0
+
+            if i == 0:
+                expected = np.array([[0.0, 
+                                    0.70710678,
+                                    -0.70710678,
+                                    0.0,
+                                    0.0,
+                                    0.0,
+                                    0.0]])
+                assert(np.isclose(h.T, expected).all())
+
+            if i == 5:
+                expected = np.array([[0.43643578,
+                                    0.43643578,
+                                    0.43643578,
+                                    -0.32732684,
+                                    -0.32732684,
+                                    -0.32732684,
+                                    -0.32732684]])
+                assert(np.isclose(h.T, expected).all())
+
+    def test_get_lilmat_and_shl(self):
+
+        t2 = get_tree_from_file('small-tree.tree')
+        t2, lilmat, shl = initiate_values(t2)
+
+        for i, node in enumerate(t2.non_tips(include_self=True)):
+            if i == 0:        
+                nontip_inds0 = None
+                nontip_inds1 = None
+                tip_inds0 = get_tip_indeces(node, 'left')
+                tip_inds1 = get_tip_indeces(node, 'right')
+                left, right = get_L(tip_inds0, tip_inds1)
+                h = get_haarvec(tip_inds0, tip_inds1, left, right, node.ntips_in_tree)
+                assert((np.isclose(sum(h), 0.0)).all()) # The sum of haar vectors should be 0
+
+                lilmat, shl = get_lilmat_and_shl(node, nontip_inds0, nontip_inds1, lilmat, shl, i)
+
+                val = 1/np.sqrt(2)
+                expected_shl = [[0, val, -val, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0]]
+
+                expected_lilmat = [[0, 1, 1, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0]]
+
+                expected_shl = scipy.sparse.lil_matrix(expected_shl)  
+                expected_lilmat = scipy.sparse.lil_matrix(expected_lilmat)  
+
+                assert(np.isclose(expected_shl.todense(), shl.todense()).all())
+                assert(np.isclose(expected_lilmat.todense(), lilmat.todense()).all())
+
+    def test_handle_case(self):
+
+        t2 = get_tree_from_file('small-tree.tree')
+        t2, lilmat, shl = initiate_values(t2)
+
+        for i, node in enumerate(t2.non_tips(include_self=True)):
+            if i == 0:        
+                lilmat, shl = handle_both(node, lilmat, shl, i)
+
+                val = 1/np.sqrt(2)
+                expected_shl = [[0, val, -val, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0]]
+
+                expected_lilmat = [[0, 1, 1, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0]]
+
+                expected_shl = scipy.sparse.lil_matrix(expected_shl)  
+                expected_lilmat = scipy.sparse.lil_matrix(expected_lilmat)  
+
+                assert(np.isclose(expected_shl.todense(), shl.todense()).all())
+                assert(np.isclose(expected_lilmat.todense(), lilmat.todense()).all())
+
+    def test_sparsify(self):
+
+        lilmat, shl = sparsify(self.tree_file)
+
+        expected_lilmat = [[0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+                           [1.0, 3.0, 3.0, 0.0, 0.0, 0.0, 0.0],
+                           [0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+                           [0.0, 0.0, 0.0, 0.0, 3.0, 3.0, 1.0],
+                           [0.0, 0.0, 0.0, 1.0, 6.0, 6.0, 4.0],
+                           [4.0, 6.0, 6.0, 5.0, 10.0, 10.0, 8.0],
+                           [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
+
+        expected_shl = [[0.0, 0.70710678, -0.70710678, 0.0, 0.0, 0.0, 0.0],
+                        [0.81649658, -0.40824829, -0.40824829, 0.0,  0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.70710678,-0.70710678, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.40824829, 0.40824829, -0.81649658],
+                        [0.0, 0.0, 0.0, 0.8660254, -0.28867513, -0.28867513, -0.28867513],
+                        [0.43643578, 0.43643578, 0.43643578, -0.32732684, -0.32732684, -0.32732684, -0.32732684],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
+
+        expected_lilmat = scipy.sparse.lil_matrix(expected_lilmat)  
+        expected_shl = scipy.sparse.lil_matrix(expected_shl)  
+
+        assert(np.isclose(expected_lilmat.todense(), lilmat.todense()).all())
+        assert(np.isclose(expected_shl.todense(), shl.todense()).all())
